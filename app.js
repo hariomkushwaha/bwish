20d5796456c803c5058b46d9087858dd7fcf1754
hariomkushwaha <hari2018cs048abesit@gmail.com>
2020-09-17 07:20:44 +0530

    birthday

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs= require("ejs");
+
+const app=express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get('/',function(req,res){
+    res.render("index");
+})
+
+app.get('/song',function(req,res){
+    res.render("song");
+})
+app.get('/book',function(req,res){
+    res.render("book");
+})
+app.get('/bdh2',function(req,res){
+    res.render("bdh2");
+})
+app.get('/last',function(req,res){
+    res.render("last");
+})
+
+app.post('/',function(req,res){
+    const name = req.body.puzz;
+    console.log(name);
+    if(req.body.puzz === "khushi"||"khushboo"){
+        res.redirect("/song");
+    }else{
+        res.redirect('/');
+    }
+})
+
+app.listen(3000,function(){
+    console.log("server is running");
+})
